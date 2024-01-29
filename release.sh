8aa69931143a95c0b9e3318bb58f13d64481119e
灵轮 <linglun.zh@alibaba-inc.com>
2024-01-29 11:24:23 +0800

    Print must read

--- release.sh
+++ release.sh
@@ -15,11 +15,10 @@ regions=(
     'eu-central-1'
     'us-east-1'
 )
-for r in ${regions[@]}
-do
+for r in ${regions[@]}; do
     export REGION=$r
     echo $REGION
-    s cli fc layer publish --layer-name webide-custom-plugin --code ide-plugin-layer.zip --compatible-runtime nodejs16,python3.10,custom.debian10,custom,nodejs14,nodejs12,nodejs10,nodejs8,nodejs6,python3,python3.9,python2.7,php7.2,java8,java11,dotnetcore2.1,go1 --region $REGION -a fc-console
+    s cli fc layer publish --layer-name webide-custom-plugin --code ide-plugin-layer.zip --compatible-runtime nodejs20,nodejs18,nodejs16,python3.10,custom.debian10,custom,nodejs14,nodejs12,nodejs10,nodejs8,nodejs6,python3,python3.9,python2.7,php7.2,java8,java11,dotnetcore2.1,go1 --region $REGION -a fc-console
     s cli fc layer acl --layer-name webide-custom-plugin --public --region $REGION -a fc-console
-    s cli fc layer detail --layer-name webide-custom-plugin --version-id latest  --region $REGION  -a fc-console
+    s cli fc layer detail --layer-name webide-custom-plugin --version-id latest --region $REGION -a fc-console
 done
--- test-plugin/extension.js
+++ test-plugin/extension.js
@@ -23,6 +23,12 @@ function activate(context) {
       },
       vscode.ConfigurationTarget.Global
     );
+
+    if(process.env.WEBIDE_FUNCTION_TYPE !== "small_account"){
+      vscode.window.onDidOpenTerminal((terminal) => {
+        terminal.sendText("clear && printf '\\n使用必读：\\n\\n1. 在此 WebIDE 中，无法直接测试层的挂载以及 OSS、NAS 的挂载，也无法在 WebIDE 中测试通过 VPC 访问对应资源。您需要在“实例列表”中“登录实例”来测试真实环境。\\n2. 安装 Python 依赖时必须添加 -t . 才能正确将依赖安装到中。例如：pip install flask -t .\\n\\n'");
+      });
+    }
   }
 
   vscode.window.showTextDocument(vscode.Uri.file("/code/index.js"));
